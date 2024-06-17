abe15b6cc866c9daba2a2eeaa7d7233e07186894
Victor Emanuel <victoremanuel12@hotmail.com>
2024-06-16 21:19:39 -0300

    refactor: move duplicated code to a helper method

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,6 @@
+import { HttpResponse } from '../protocols/http';
+// () apos a => ({}) o type entende que é um return
+export const badRequest = (error : Error) :  HttpResponse => ({ 
+        statusCode:400, 
+        body: error
+})
