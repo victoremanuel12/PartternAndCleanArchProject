4a3cd24a61e5bb05615e333e6b567a76d6e491fd
Victor Emanuel <victoremanuel12@hotmail.com>
2024-06-24 22:02:18 -0300

    feat: create serverErrorHelper

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,11 @@
+import { ServerError } from '../erros/server.error';
 import { HttpResponse } from '../protocols/http';
 // () apos a => ({}) o type entende que é um return
 export const badRequest = (error : Error) :  HttpResponse => ({ 
     statusCode:400, 
     body: error
+})
+export const serverError = () :  HttpResponse => ({ 
+    statusCode:400, 
+    body: new ServerError()
 })
