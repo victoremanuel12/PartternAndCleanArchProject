fac722260e6809e9c573e941fdff7fabaca5ffab
Victor Emanuel <victoremanuel12@hotmail.com>
2024-07-07 17:44:29 -0300

    refactor: muve sut creation to a factor helperMethod

--- src/utils/email.validator-adapter.spec.ts
+++ src/utils/email.validator-adapter.spec.ts
@@ -5,20 +5,23 @@ jest.mock('validator', () => ({
         return true
     }
 }))
+const makeSut = () : EmailValidatorAdapter =>  {
+    return new EmailValidatorAdapter();
+}
 describe("EmailValidator Adpter", () => {
     test("Should return false if validator returns false", () => {
-        const sut = new EmailValidatorAdapter()
+        const sut =  makeSut()
         jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
         const isValid = sut.isValid("invalid_email@gmail.com")
         expect(isValid).toBe(false) 
     })
     test("Should return true if validator returns true", () => {
-        const emailValidator = new EmailValidatorAdapter()
+        const emailValidator = makeSut()
         const isValid = emailValidator.isValid("valid_email@gmail.com")
         expect(isValid).toBe(true)
     })
     test("Should call  validator with correct email", () => {
-        const sut = new EmailValidatorAdapter();
+        const sut = makeSut();
         const isEmailSpy = jest.spyOn(validator, 'isEmail');
         sut.isValid("valid_email@gmail.com")
         expect(isEmailSpy).toHaveBeenCalledWith("valid_email@gmail.com")
